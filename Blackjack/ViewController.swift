7aa9587f4200a4f4eff8570f2019e1ad6e74f6b3
George <47723687+george-cochrane@users.noreply.github.com>
2024-04-14 21:56:48 +0100

    adding blackjack code, and card image icons etc

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgCar2: UIImageView!
+    
+    @IBOutlet weak var imgCard1: UIImageView!
+    
+    @IBAction func btnDeal(_ sender: Any) {
+    }
+    
+    func createDeck() -> [String]{
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        
+        var blankDeck: [String] = []
+        
+        for suit in suits{
+            for rank in 1...13 {
+                if rank < 10{
+                    let newCard = suit + "0" + String(rank)
+                    blankDeck.append(newCard)
+                } else {
+                    let newCard = suit + String(rank)
+                    blankDeck.append(newCard)
+                }
+            }
+        }
+        return blankDeck
+        
+    }
+    
+    var deck: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        deck = createDeck()
+              
+        
     }
-
-
 }
-
