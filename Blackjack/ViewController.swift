5566f8042ff1bc0eade9311719a278b90e797c5b
George <47723687+george-cochrane@users.noreply.github.com>
2024-04-14 22:57:09 +0100

    additional cards add

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     var myDeck: [String] = []
-
+    var dealerCount = 0
+    var playerCount = 0
+    var turn = true
     
     @IBOutlet weak var imgCard1: UIImageView!
-    
     @IBOutlet weak var imgCard2: UIImageView!
     
     @IBAction func btnDeal(_ sender: Any) {
         imgCard1.image = UIImage(named: randomCard(deck: myDeck))
         imgCard2.image = UIImage(named: randomCard(deck: myDeck))
+        
+        createCard()
     }
-
+    
     func randomCard(deck: [String]) -> String{
         
         let maxRange = deck.count - 1
@@ -47,6 +50,30 @@ class ViewController: UIViewController {
         return blankDeck
         
     }
+    
+    func createCard(){
+        let cardView = UIImageView()
+        
+        if turn == true && dealerCount < 5{
+
+            cardView.frame = CGRect(x: 50 + (dealerCount * 50), y: 100, width: 60, height: 90)
+            cardView.image = UIImage(named: myDeck.removeLast())
+            
+            dealerCount += 1
+            turn = false
+            
+        } else if turn == false && playerCount < 5{
+            cardView.frame = CGRect(x: 50 + (playerCount * 50), y: 300, width: 60, height: 90)
+            cardView.image = UIImage(named: myDeck.removeLast())
+            dealerCount += 1
+            turn = true
+        
+        } else {
+        print("No more cards being dealt")
+    }
+        view.addSubview(cardView)
+}
+    
     
     var deck: [String] = []
     
